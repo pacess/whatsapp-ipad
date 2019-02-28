374b6e3655cc0ed9747a51429c11f40883af1f9b
Pacess HO <pacessho@fimmick.com>
2019-02-28 17:31:59 +0800

    Better UI/UX

--- WhatsApp/ViewController.m
+++ WhatsApp/ViewController.m
@@ -15,7 +15,9 @@
 //  Do any additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad  {
 	[super viewDidLoad];
-	NSString *userAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15";
+	NSString *chromeAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36";
+	NSString *safariAgent = @"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15";
+	NSString *userAgent = safariAgent;
 
 	NSURL *url = [NSURL URLWithString:@"https://web.whatsapp.com/"];
 	NSURLRequest *request = [NSURLRequest requestWithURL:url];
@@ -24,6 +26,7 @@
 	WKWebView *webview = [[WKWebView alloc] initWithFrame:rect];
 	[webview setCustomUserAgent:userAgent];
 	[webview loadRequest:request];
+	[webview.scrollView setScrollEnabled:NO];
 	[self.view addSubview:webview];
 	_webview = webview;
 }
